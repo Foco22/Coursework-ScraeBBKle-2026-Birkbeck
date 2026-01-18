d8bc4da9613f33bc42b92c91280a4ae0df367c6b
Francisco Macaya <francisco.macaya22@gmail.com>
2026-01-18 13:19:19 +0000

    add a computer move it to contain all the movement for the computer

---
+++ src/main/pij/service/ComputerMove.java
@@ -0,0 +1,57 @@
+package main.pij.service;
+
+import main.pij.model.Board;
+import main.pij.model.Bag;
+import main.pij.model.Player;
+import main.pij.model.WordList;
+import main.pij.model.WordCells;
+import main.pij.model.Cell;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * Manages the Scrabble game flow.
+ */
+public class ComputerMove {
+    private final Board board;
+    private final Player player1;
+    private final Player player2;
+    private final Bag bag;
+
+
+    public ComputerMove(Board board, Player player1, Player player2, Bag bag) {
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.bag = bag;
+    }
+    
+
+    public class Permutations {
+
+        public static void permute(String prefix, String remaining) {
+            if (prefix.length() == 3) {
+                System.out.println(prefix);
+                return;
+            }
+    
+            for (int i = 0; i < remaining.length(); i++) {
+                permute(
+                    prefix + remaining.charAt(i),
+                    remaining.substring(0, i) + remaining.substring(i + 1)
+                );
+            }
+        }
+    
+        public static void main(String[] args) {
+            permute("", "arun");
+        }
+    }
+    
+
+}
